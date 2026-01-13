329af6fd1b65436b0a2cc40054cf360e463b0e1a
Muhammad Hadi Royan Firdaus <hadiroyan3@gmail.com>
2026-01-13 14:20:44 +0700

    feat (BE): add security dependency and fix entity refresh_tokens

--- backend/src/main/java/com/auth/backend/entity/RefreshToken.java
+++ backend/src/main/java/com/auth/backend/entity/RefreshToken.java
@@ -3,14 +3,26 @@ package com.auth.backend.entity;
 import java.time.Instant;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "refresh_tokens")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RefreshToken {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
